29f7ed6a62b75e2268314565b6fdf588d9f783f6
CesarAntolinez <cesaralejandroantolinez@gmail.com>
2020-03-07 23:26:26 -0500

    Servicios http

---
+++ src/app/services/http.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) { }
+
+  post(serviceName: string, data: any) {
+    const headers = new HttpHeaders();
+    const options = { headers: headers, withCredintials: false };
+    const url = environment.apiUrl + serviceName;
+
+    return this.http.post(url, JSON.stringify(data), options);
+  }
+  put(serviceName: string, data: any) {
+    const headers = new HttpHeaders();
+    const options = { headers: headers, withCredintials: false };
+    const url = environment.apiUrl + serviceName;
+
+    return this.http.put(url, JSON.stringify(data), options);
+  }
+  delete(serviceName: string, data: any) {
+    const headers = new HttpHeaders();
+    const options = { headers: headers, withCredintials: false };
+    const url = environment.apiUrl + serviceName;
+
+    return this.http.delete(url, options);
+  }
+}
